677833241df1dd85c89a4ec667511721bd50f3a7
Dena <dena-a@users.noreply.github.com>
2023-12-27 13:12:17 +0330

    Update test routes.php

--- src/Gateways/Test/routes.php
+++ src/Gateways/Test/routes.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 if (app('config')->get('iranpayment.test.active', false) &&
     app('config')->get('iranpayment.test.url') !== null) {
-    Route::post(
+    Route::any(
         app('config')->get('iranpayment.test.url'),
         'Dena\IranPayment\Gateways\Test\TestGatewayController@paymentView'
     )->name('iranpayment.test.pay');
